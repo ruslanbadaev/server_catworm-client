fe6ad35c131e1c7e0685650c8aa65978c82703c0
ruslanbadaev <r.badaeff@gmail.com>
2021-05-26 21:12:29 +0300

    remove notification data

---
+++ lib/notifiers/notification_notifier copy.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'package:server_catworm/models/scanner.dart';
+
+class NotificationNotifier with ChangeNotifier {
+  // Crude counter to make messages unique
+  int _messageCount = 0;
+
+  /// The API endpoint here accepts a raw FCM payload for demonstration purposes.
+  String constructFCMPayload(String token) {
+    _messageCount++;
+    return jsonEncode({
+      'token': token,
+      'data': {
+        'via': 'FlutterFire Cloud Messaging!!!',
+        'count': _messageCount.toString(),
+      },
+      'notification': {
+        'title': 'Hello FlutterFire!',
+        'body': 'This notification (#$_messageCount) was created via FCM!',
+      },
+    });
+  }
+}
